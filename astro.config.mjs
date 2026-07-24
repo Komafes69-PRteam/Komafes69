90daa59b2d85b4124e58ccbafee4c81065f0fe3c
TakumaYoshii <ysitkm304209@gmail.com>
2026-07-24 21:03:19 +0900

    github pages settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
 
-// https://astro.build/config
 export default defineConfig({
+  site: 'https://komafes69-prteam.github.io',
+  base: '/Komafes69',
+
   vite: {
     plugins: [tailwindcss()]
   }
