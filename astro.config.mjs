bbf8cb0a6648a98b24ae7a98d598462044f8ebe6
reiwanopikaso <ysitkm304209@gmail.com>
2026-07-31 00:26:07 +0900

    add detail

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   site: 'https://komafes69-prteam.github.io',
-  base: '/Komafes69',
+  base: '/',
 
   vite: {
     plugins: [tailwindcss()]
