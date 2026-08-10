34f72aa0490f2f9fe5d7ff9d120a237643517ab7
TakumaYoshii <ysitkm304209@gmail.com>
2026-08-10 16:45:33 +0900

    add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,15 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
+import sitemap from '@astrojs/sitemap';
+
 export default defineConfig({
   site: 'https://69.komafes.jp',
-  
+
   // Cloudflareのアダプター設定(adapter: cloudflare()など)があればすべて削除
   vite: {
     plugins: [tailwindcss()],
   },
+
+  integrations: [sitemap()],
 });
